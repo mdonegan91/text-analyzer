07683c960fd4e28258cd3dcac43d1997c0f64898
Molly Donegan <mollyrdonegan@gmail.com>
2023-01-18 18:11:59 -0800

    add more bad words to omit

--- README.md
+++ README.md
@@ -77,3 +77,33 @@ Describe: numberOfOccurencesInText()
         const text = "red RED Red!";
         numberOfOccurrencesInText(word, text);
     Expected Output: 0
+
+Describe: boldPassage()
+
+    Test: "It should return null if no word or text is entered."
+    Code:
+        const text = "";
+        const word = "";
+        boldPassage(word, text);
+    Expected Output: null
+
+    Test: "It should return a non-matching word in a p tag."
+    Code:
+        const word = "hello";
+        const text = "yo";
+        boldPassage(word, text);
+    Expected Output: <p>yo</p>
+
+    Test: "It should return a matching word in a strong tag."
+    Code:
+        const word = "hello";
+        const text = "hello";
+        boldPassage(word, text);
+    Expected Output: <p><strong>hello</strong></p>
+
+    Test: "It should wrap words that match in strong tags but not words that don't."
+    Code:
+        const word = "hello";
+        const text = "hello there";
+        boldPassage(word, text);
+    Expected Output: <p><strong>hello</strong> there</p>
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
+// Utility Logic
+
+function isEmpty(testString) {
+    return (testString.trim().length === 0);
+}
+
 // Business Logic
 
 function wordCounter(text) {
-    if (text.trim().length === 0) {
+    if (isEmpty(text)) {
         return 0;
     }
     let wordCount = 0;
@@ -15,20 +21,41 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
-    if (word.trim().length === 0) {
+    if (isEmpty(word)) {
         return 0;
     }
     const textArray = text.split(" ");
     let wordCount = 0;
     textArray.forEach(function (element) {
         if (element.toLowerCase().includes(word.toLowerCase())) {
-            wordCount++
+            wordCount++;
         }
     });
     return wordCount;
 }
 
 // UI Logic
+
+function boldPassage(word, text) {
+    if (isEmpty(word) || isEmpty(text)) {
+        return null;
+    }
+    const p = document.createElement("p");
+    let textArray = text.split(" ");
+    textArray.forEach(function (element, index) {
+        if (word === element) {
+            const bold = document.createElement("strong");
+            bold.append(element);
+            p.append(bold);
+        } else {
+            p.append(element);
+        }
+        if (index !== (textArray.length - 1)) {
+            p.append(" ");
+        }
+    });
+    return p;
+}
 
 function handleFormSubmission() {
     event.preventDefault();
@@ -38,6 +65,12 @@ function handleFormSubmission() {
     const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
     document.getElementById("total-count").innerText = wordCount;
     document.getElementById("selected-count").innerText = occurrencesOfWord;
+    let boldedPassage = boldPassage(word, passage);
+    if (boldedPassage) {
+        document.querySelector("div#bolded-passage").append(boldedPassage);
+    } else {
+        document.querySelector("div#bolded-passage").innerText = null;
+    }
 }
 
 window.addEventListener("load", function () {
